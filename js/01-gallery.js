1e0ef861d0d3cf38d61e0012148edbbfccb10764
annafrymark <anna.frymark@gmail.com>
2022-12-29 23:12:58 +0100

    part 1 working

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,20 +1,33 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-galleryItems.forEach(function (galleryItem, index) {
+galleryItems.forEach(function (galleryItem) {
   const div = document.createElement("div");
-  document.body.appendChild(div);
   div.classList.add("gallery__item");
   const a = document.createElement("a");
   div.appendChild(a);
   a.classList.add("gallery__link");
-  a.setAttribute("href", "original");
+  a.setAttribute("href", galleryItem.original);
   const img = document.createElement("img");
   a.appendChild(img);
   img.classList.add("gallery__image");
-  img.setAttribute("src", "preview");
-  img.setAttribute("data-source", "original");
-  img.setAttribute("alt", "description");
+  img.setAttribute("src", galleryItem.preview);
+  img.setAttribute("data-source", galleryItem.original);
+  img.setAttribute("alt", galleryItem.description);
+  document.querySelector("div.gallery").appendChild(div);
 });
 
 console.log(galleryItems);
+
+document.querySelector("div.gallery").addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log(event.target.getAttribute("data-source"));
+  const instance = basicLightbox.create(`
+  <img src = ${event.target.dataset.source}>
+ 
+`);
+  console.log(event.target.alt);
+  instance.show();
+});
+
+
